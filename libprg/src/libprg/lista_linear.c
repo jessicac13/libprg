1e2959628e60356534202d7136a91b4e9eb68680
jessica <jessica.c13@aluno.ifsc.edu.br>
2024-03-06 16:04:58 -0300

    alterações da algoritmo de busca ajuste

--- libprg/src/libprg/lista_linear.c
+++ libprg/src/libprg/lista_linear.c
@@ -68,12 +68,14 @@ int busca_bin_r(int lista[], int inicio, int fim, int alvo)
     }
     return false;
 }
-void inserir_desodernado(int lista[] , int tamanho_lista, int valor)
+int *inserir_desodernado(int *lista , int tamanho_lista, int valor)
  {
     tamanho_lista++;
 
     lista = realloc(lista, tamanho_lista * sizeof (int));
 
     lista[tamanho_lista-1]= valor;
+
+     return lista;
 
  }
