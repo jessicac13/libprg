1eca775bd2eada56084a9d2138be14a92a53a09f
jessica <jessica.c13@aluno.ifsc.edu.br>
2024-03-06 11:40:21 -0300

    alterações da algoritmo de busca

--- libprg/src/include/libprg/libprg.h
+++ libprg/src/include/libprg/libprg.h
@@ -11,5 +11,9 @@ typedef enum { SUM, SUB } operation_t;
 result_t sub(double a, double b);
 result_t sum(double a, double b);
 result_t compute(double a, double b, operation_t op);
+int busca_lista_desodernada(int lista[], int tamanho_lista, int alvo);
+int busca_lista_odernada(int lista[], int tamanho_lista, int alvo);
+int busca_bin_i(int lista[], int tamanho_lista, int alvo);
+int busca_bin_r(int lista[], int inicio, int fim, int alvo);
 
 #endif
