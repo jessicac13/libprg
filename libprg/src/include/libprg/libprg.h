a5de673271b71da7cada6f0acdd79ca479036ac5
jessica <jessica.c13@aluno.ifsc.edu.br>
2024-03-12 16:51:57 -0300

    alterações da algoritmo de busca ajuste

--- libprg/src/include/libprg/libprg.h
+++ libprg/src/include/libprg/libprg.h
@@ -1,8 +1,8 @@
 #ifndef LIBPRG_LIBPRG_H
 #define LIBPRG_LIBPRG_H
 
-#include <stdio.h>
-#include <stdlib.h>
+//#include <stdio.h>
+//#include <stdlib.h>
 #include<stdbool.h>
 //#include <libprg/libprg.h>
 
@@ -11,6 +11,13 @@ typedef struct {
     int error;
 } result_t;
 
+typedef enum { SUM, SUB } operation_t;
+
+result_t sub(double a, double b);
+result_t sum(double a, double b);
+result_t compute(double a, double b, operation_t op);
+
+// Definição da estrutura lista_t
 typedef struct {
     int *elemento;
     int tamanho;
@@ -18,20 +25,13 @@ typedef struct {
     bool ordenada;
 } lista_t;
 
-typedef enum { SUM, SUB } operation_t;
-
-result_t sub(double a, double b);
-result_t sum(double a, double b);
-result_t compute(double a, double b, operation_t op);
-
-void criarlista(lista_t *lista, int capacidade);
-void inserirlistaDesodernada(lista_t *lista, int elemento);
-void inserirlistaOdernada(lista_t *lista, int elemento);
+void criarlista(lista_t *lista, int capacidade, bool ordenada);
+void inserirlista(lista_t *lista, int elemento);
 void imprimirlista(lista_t *lista);
+
+#endif /* LIBPRG_H */
 
 //int busca_lista_desodernada(struct lista_t lista, int alvo);
 //int busca_lista_odernada(struct lista_t lista, int alvo);
 //int busca_bin_i(struct lista_t lista, int alvo);
 //int busca_bin_r(struct lista_t lista, int inicio, int fim, int alvo);
-
-#endif
