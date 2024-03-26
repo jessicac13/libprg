b0ea4b1f577bf15340ec4b2a268652bb2b10ee84
jessica <jessica.c13@aluno.ifsc.edu.br>
2024-03-26 14:12:53 -0300

    alterações da algoritmo de busca ajuste

--- libprg/src/libprg/fila_circular.c
+++ libprg/src/libprg/fila_circular.c
@@ -37,7 +37,7 @@ fila_t* criarFila(int capacidade)
 
 void enqueue(fila_t *fila, int elemento)
 {
-    if (fila->tamanho <= fila->capacidade)
+    if (fila->tamanho <= fila->capacidade && fila->inicio <= fila->capacidade)
     {
         fila->elemento[fila->fim] = elemento;
         fila->tamanho++;
