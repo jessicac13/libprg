0093f09254d746d5fbe5efcffb9e34fb7a5dedd2
jessica <jessica.c13@aluno.ifsc.edu.br>
2024-03-26 18:30:55 -0300

    alterações da algoritmo de busca ajuste

--- libprg/src/libprg/fila_circular.c
+++ libprg/src/libprg/fila_circular.c
@@ -88,6 +88,7 @@ void dequeue(fila_t *fila)
     int elementoRemovido = fila->elemento[fila->inicio];
 
     fila->inicio = (fila->inicio + 1) % fila->capacidade;
+    fila->fim = (fila->fim + 1) % fila->capacidade;
 
     fila->tamanho--;
 
