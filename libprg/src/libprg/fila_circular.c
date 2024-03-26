60252746bb229da07e54a89051006313e62d200e
jessica <jessica.c13@aluno.ifsc.edu.br>
2024-03-26 19:59:26 -0300

    alterações da algoritmo de busca ajuste

--- libprg/src/libprg/fila_circular.c
+++ libprg/src/libprg/fila_circular.c
@@ -105,14 +105,8 @@ int head(fila_t  *fila)
 
 int tail(fila_t  *fila)
 {
-    if(fila->fim > 0)
-    {
-        return fila->elemento[(fila->fim) - 1];
-    }
-    else
-    {
+
         return fila->elemento[fila->fim];
-    }
 }
 
 //tail();
