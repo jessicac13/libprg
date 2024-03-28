41ea57c1aeebfe7d1248597809a89446caff5f54
jessica <jessica.c13@aluno.ifsc.edu.br>
2024-03-28 14:31:22 -0300

    alterações da algoritmo de busca ajuste

--- libprg/src/libprg/lista_linear.c
+++ libprg/src/libprg/lista_linear.c
@@ -131,11 +131,10 @@ int buscaLista(lista_t *lista, int alvo)
 }
 
 
-
 int busca_bin_i(lista_t *lista, int alvo)
 {
     int meio;
-    int inicio = 0;
+    int inicio = 1;
     int fim = lista->tamanho;
     while (inicio <= fim)
     {
