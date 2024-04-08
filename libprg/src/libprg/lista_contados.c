bf04fce8170ed57db2ee5b0d46b001faa3bfee12
jessica <jessica.c13@aluno.ifsc.edu.br>
2024-04-08 19:41:10 -0300

    alterações da algoritmo de busca ajuste

--- libprg/src/libprg/lista_contados.c
+++ libprg/src/libprg/lista_contados.c
@@ -93,20 +93,6 @@ void inserirListaContatos(lista_t *lista, char nome[MAX_NOME], char telefone[MAX
     }
 }
 
-void imprimirListaContatos(lista_t *lista)
-{
-    printf("\nLista de Contatos:\n");
-
-    for (int i = 0; i < lista->tamanho; i++)
-    {
-        printf("Contato %d:\n", i + 1);
-        printf("Nome: %s\n", lista->elemento[i].nome);
-        printf("Telefone: %s\n", lista->elemento[i].telefone);
-        printf("Email: %s\n", lista->elemento[i].email);
-        printf("\n");
-    }
-}
-
 void removerListaContatos(lista_t *lista, char alvo[MAX_NOME])
 {
     int indice = buscaListaContatos(lista, alvo);
@@ -234,40 +220,4 @@ void editarContatoNome(lista_t *lista, char alvo[MAX_NOME], char nome[MAX_NOME])
     }
 }
 
-int salvarArquivo(struct lista_t *lista, char diretorio[1000])
-{
-    FILE *arquivo = fopen(diretorio, "w");
 
-    if (arquivo == NULL) {
-        perror("Erro ao abrir o arquivo");
-        return 1;
-    }
-
-    for (int i = 0; i < lista->tamanho; i++) {
-        fprintf(arquivo, "%s,%s,%s\n", lista->elemento[i].nome, lista->elemento[i].telefone, lista->elemento[i].email);
-    }
-
-    fclose(arquivo);
-
-    return 0;
-}
-
-void recarregarListaContatos(lista_t *lista, const char *nome_arquivo)
-{
-    FILE *arquivo = fopen(nome_arquivo, "r");
-    if (arquivo == NULL)
-    {
-        perror("Erro ao abrir o arquivo");
-        return; // Encerra a função se não for possível abrir o arquivo
-    }
-
-    char linha[MAX_NOME + MAX_TELEFONE + MAX_EMAIL + 3]; // Tamanho máximo de uma linha
-
-    while (fgets(linha, sizeof(linha), arquivo) != NULL) {
-        char nome[MAX_NOME], telefone[MAX_TELEFONE], email[MAX_EMAIL];
-        sscanf(linha, "%[^,],%[^,],%s", nome, telefone, email);
-        inserirListaContatos(lista, nome, telefone, email);
-    }
-
-    fclose(arquivo); // Fecha o arquivo
-}
