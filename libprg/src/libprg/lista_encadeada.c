86a0f3956d0d6a65fe4594bdb82401602c58b1e5
jessica <jessica.c13@aluno.ifsc.edu.br>
2024-04-15 11:27:18 -0300

    acrescentando lista encadeada

--- libprg/src/libprg/lista_encadeada.c
+++ libprg/src/libprg/lista_encadeada.c
@@ -5,10 +5,10 @@
 #include <string.h>
 #include <libprg/libprg.h>
 
-struct no_t{
+typedef struct no_t{
     int dado;
     struct no* proximo;
-};
+} no_t;
 
 void adicionar(no_t** inicio, int dado)
 {
